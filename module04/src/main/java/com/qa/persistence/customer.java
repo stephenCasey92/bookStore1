3f234038e6638298b5fe8a03f12f6cf84ede33db
imannion12 <manyon05@outlook.com>
2017-01-27 15:36:15 +0000

    added Customer entity

---
+++ module04/src/main/java/com/qa/persistence/customer.java
@@ -0,0 +1,89 @@
+package com.qa.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity 
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class customer {
+	
+	@Id
+	@GeneratedValue
+	protected Long id;
+	
+	@Column (length = 50)
+	protected String name;
+	
+	@Column (length = 50)
+	protected char email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char getEmail() {
+		return email;
+	}
+
+	public void setEmail(char email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + email;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		customer other = (customer) obj;
+		if (email != other.email)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "customer [id=" + id + ", name=" + name + ", email=" + email
+				+ "]";
+	}
+
+	
+}
